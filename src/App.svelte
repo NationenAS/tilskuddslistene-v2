5a7fcedd5eb97e5038e7d489ce32197e166829c4
Jarand Ullestad <jarand.ullestad@nationen.no>
2023-06-13 14:26:15 +0200

    Fungerende versjon med info

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,22 @@
 <script>
 
-import { onMount } from "svelte"
 import Spinner from "./Spinner.svelte"
 import Navigation from "./Navigation.svelte"
+import { fly } from 'svelte/transition'
 
 const url = 'https://services.api.no/api/acies/v1/custom/AgriculturalSubsidy?'
 
 let listData = []
+let currentQuery = ""
+let uniqueData = {}
+let uniqueBox = false
 
 function updateFromNav(event) {
     listData = []
     let params = "limit=10&sortBy=sum_produksjons_og_avloesertilskudd&fields=(id,orgnavn,sum_produksjons_og_avloesertilskudd,soeknads_aar)"
     let start = performance.now()
-    console.log("Fetching " + url + params + event.detail.params)
+    console.log("Fetching: " + url + params + event.detail.params)
+    currentQuery = url + params + event.detail.params
     fetch(url + params + event.detail.params)
         .then(r => r.json())
         .then(d => {
@@ -23,6 +27,21 @@ function updateFromNav(event) {
         })
 }
 
+function showInfo(id) {
+    uniqueBox = true
+    let params = `limit=1&equal=id:${id}&fields=(id,orgnavn,orgnr,sum_produksjons_og_avloesertilskudd,soeknads_aar)`
+    console.log("Fetching unique: " + url + params)
+    fetch(url + params)
+        .then(r => r.json())
+        .then(d => {
+            uniqueData = d[0]
+        })
+}
+function hideInfo() {
+    uniqueData = {}
+    uniqueBox = false
+}
+
 </script>
 
 <h2>Tilskuddslistene</h2>
@@ -31,18 +50,67 @@ function updateFromNav(event) {
 {#if listData.length == 0}
 <Spinner />
 {:else}
-<div class="result-table">
-    {#each listData as item}
-    <div>{item.soeknads_aar}</div>
-    <div>{item.orgnavn}</div>
-    <div>{item.sum_produksjons_og_avloesertilskudd.toLocaleString('nb-NO')}</div>
-    {/each}
+<div class="result-container">
+    <div class="result-table">
+        <div class="result-row">
+            <div>År</div>
+            <div>Navn</div>
+            <div class="result-sum">Sum</div>
+        </div>
+        {#each listData as item}
+        <div class="result-row" on:click={() => { showInfo(item.id) }} on:keypress={() => { showInfo(item.id) }}>
+            <div>{item.soeknads_aar}</div>
+            <div>{item.orgnavn}</div>
+            <div class="result-sum">{item.sum_produksjons_og_avloesertilskudd.toLocaleString('nb-NO')} kr</div>
+        </div>
+        {/each}
+    </div>
+    {#if uniqueBox }
+    <div class="result-info" in:fly="{{ x: 1000, duration: 500 }}" out:fly="{{ x: 1000, duration: 500 }}" on:click={() => { hideInfo() }}>
+        {#if Object.keys(uniqueData).length == 0}
+        <Spinner />
+        {:else}
+        <h3>{uniqueData.orgnavn}</h3>
+        <div>{uniqueData.orgnr}</div>
+        <div>Sum mottatt i {uniqueData.soeknads_aar}: {uniqueData.sum_produksjons_og_avloesertilskudd.toLocaleString('nb-NO')} kroner</div>
+        {/if}
+    </div>
+    {/if}
 </div>
 {/if}
 
 <style>
+h2 {
+    margin-bottom: 15px;
+}
+.result-container {
+    position: relative;   
+}
 .result-table {
+    margin-top: 20px;
+}
+.result-row {
     display: grid;
-
+    grid-template-columns: 1fr 4fr 1fr;
+    grid-template-rows: 30px;
+    align-items: center;
+    cursor: pointer;
+}
+.result-row:hover {
+    background: #eee;
+}
+.result-sum {
+    text-align: right;
+}
+.result-info {
+    position: absolute;
+    top: 0%;
+    height: 100%;
+    left: 50%;
+    width: 50%;
+    background: black;
+    color: white;
+    padding: 30px;
+    cursor: pointer;
 }
 </style>
--- src/Navigation.svelte
+++ src/Navigation.svelte
@@ -1,20 +1,43 @@
 <script>
+
 import { counties } from "./counties"
 import { municipalities } from "./municipalities"
 import { createEventDispatcher } from 'svelte'
+import { onMount } from "svelte"
 
 const dispatch = createEventDispatcher()
 
 let config = {
     municipality: "",
-    county: ""
+    county: "",
+    name: ""
 }
+let preventUpdate = true
 
 function getParams() {
     let paramsString = String()
     if (config.municipality != "") paramsString += `&inPolygon=geometry:municipality:${config.municipality}`
     else if (config.county != "") paramsString += `&inPolygon=geometry:county:${config.county}`
+    if (config.name != "") paramsString += `&in=orgnavn:${config.name}`
     return paramsString
+}
+
+function getCounty(municipalityNumber) {
+    let m = municipalities.find(e => e[1] == municipalityNumber)
+    return { name: m[0], number: m[1], county: m[2] }
+}
+
+function setCorrectCounty() {
+    preventUpdate = false
+    if (config.municipality != "") {
+        let m = getCounty(config.municipality)
+        if (config.county != m.county) config.county = m.county
+    }
+}
+
+function resetMunicipality() {
+    preventUpdate = false
+    config.municipality = ""
 }
 
 function update() {
@@ -22,29 +45,38 @@ function update() {
     dispatch('navigationChange', {
         params: getParams()
     })
+    preventUpdate = true
 }
 
 function handleSubmit() {
 
 }
 
-$: update(config)
+$: if (!preventUpdate) update(config)
+
+onMount(() => {
+    update()
+})
 
 </script>
 
-<form on:submit|preventDefault={handleSubmit}>
-    <select class="form-control" name="county" bind:value={config.county}>
+<form on:submit|preventDefault={() => {preventUpdate = false, update()}}>
+    <select class="form-control" name="county" bind:value={config.county} on:change={resetMunicipality}>
         <option value="">Alle fylker</option>
         {#each counties as county}
         <option value="{county[1]}">{county[0]}</option>
         {/each}
     </select>
-    <select class="form-control" name="municipality" bind:value={config.municipality}>
+    <select class="form-control" name="municipality" bind:value={config.municipality} on:change={setCorrectCounty}>
         <option value="">Alle kommuner</option>
         {#each municipalities as municipality}
+        {#if ( config.county != "" && config.county == municipality[2] ) || config.county == "" }
         <option value="{municipality[1]}">{municipality[0]}</option>
+        {/if}
         {/each}
     </select>
+    <input type="text" placeholder="Søk på navn" bind:value={config.name}>
+    <input type="submit">
 </form>
 
 <style>
